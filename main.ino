e771edec0768a4abe6ffc0251a2e11d462d8e106
Rafał Pysz <kuchasz1@o2.pl>
2017-08-27 23:35:18 +0200

    somethin broken

--- main.ino
+++ main.ino
@@ -1,37 +1,27 @@
-#include <ESP8266WiFi.h>
+bool buttonTriggerState = HIGH;
+void handleButtonState(){
+    auto buttonState = digitalRead(10);
+    if(buttonTriggerState != buttonState){
+        if (buttonState == LOW){
+            digitalWrite(D1, HIGH);
+            digitalWrite(D2, HIGH);
+            // tone(D6, 1000);
+        } else {
+            digitalWrite(D1, LOW);
+            digitalWrite(D2, LOW);
+            // noTone(D6);
+        }
+    }
+    buttonTriggerState = buttonState;
+}
 
 void setup(){
     Serial.begin(115200);
-
     pinMode(10, INPUT);
+    pinMode(D1, OUTPUT);
     pinMode(D2, OUTPUT);
-
-    // WiFi.begin("NETUS_FIBER_STRAZ", "straz123456");
-
-
-    // Serial.println("Connecting to WiFi network");
-    // while (WiFi.status() != WL_CONNECTED){
-    //     delay(500);
-    //     Serial.print(".");
-    // }
-
-    // Serial.println("Connected!");
 }
 
-int lastCode = 1;
 void loop(){
-    auto code = digitalRead(10);
-
-    if(lastCode != code){
-        if (code == LOW){
-            digitalWrite(D2, HIGH);
-            tone(D6, 1000);
-        } else {
-            digitalWrite(D2, LOW);
-            noTone(D6);
-            
-        }
-    }
-
-    lastCode = code;
+    handleButtonState();
 }

9c14aaa5c075cd967c5c77cdb36ba9d7b1e7de85
Rafal Pysz <kuchasz1@o2.pl>
2017-08-27 12:17:24 +0200

    button handled

--- main.ino
+++ main.ino
@@ -2,18 +2,23 @@
 
 void setup(){
     Serial.begin(115200);
-    WiFi.begin("NETUS_FIBER_STRAZ", "straz123456");
+
+    pinMode(10, INPUT);
+    pinMode(D2, OUTPUT);
+
+    // WiFi.begin("NETUS_FIBER_STRAZ", "straz123456");
 
 
-    Serial.println("Connecting to WiFi network");
-    while (WiFi.status() != WL_CONNECTED){
-        delay(500);
-        Serial.print(".");
-    }
+    // Serial.println("Connecting to WiFi network");
+    // while (WiFi.status() != WL_CONNECTED){
+    //     delay(500);
+    //     Serial.print(".");
+    // }
 
-    Serial.println("Connected!");
+    // Serial.println("Connected!");
 }
 
 void loop(){
-    
+    auto code = digitalRead(10);
+    digitalWrite(D2, code);
 }

47772852bdd8f5c4c59c04289593fc7fb6fd8901
Rafal Pysz <kuchasz1@o2.pl>
2017-08-30 20:33:55 +0200

    handled button and LEDs, playing random sounds

--- main.ino
+++ main.ino
@@ -1,15 +1,41 @@
-bool buttonTriggerState = HIGH;
+int getOneOrZero(){
+    return rand()%2;
+}
+
+int* getDiodeToUse(int* arrStates) {
+    arrStates[0] = getOneOrZero();
+    arrStates[1] = getOneOrZero();
+    arrStates[2] = getOneOrZero();
+    return arrStates;
+}
+
+int getTune(){
+    int tunes[] = {262, 294, 330, 349, 392, 440, 494, 523};
+    return tunes[rand()%8];
+}
+
+int buttonTriggerState = HIGH;
+
 void handleButtonState(){
     auto buttonState = digitalRead(10);
     if(buttonTriggerState != buttonState){
         if (buttonState == LOW){
-            digitalWrite(D1, HIGH);
-            digitalWrite(D2, HIGH);
-            // tone(D6, 1000);
+
+            int* diodeStates = getDiodeToUse(new int[3]);
+
+            Serial.println(diodeStates[0]);
+            Serial.println(diodeStates[1]);
+            Serial.println(diodeStates[2]);
+
+            digitalWrite(D6, diodeStates[0]);
+            digitalWrite(D7, diodeStates[1]);
+            digitalWrite(D8, diodeStates[2]);
+            tone(D0, getTune());
         } else {
-            digitalWrite(D1, LOW);
-            digitalWrite(D2, LOW);
-            // noTone(D6);
+            digitalWrite(D6, LOW);
+            digitalWrite(D7, LOW);
+            digitalWrite(D8, LOW);
+            noTone(D0);
         }
     }
     buttonTriggerState = buttonState;
@@ -18,8 +44,10 @@ void handleButtonState(){
 void setup(){
     Serial.begin(115200);
     pinMode(10, INPUT);
-    pinMode(D1, OUTPUT);
-    pinMode(D2, OUTPUT);
+    pinMode(D0, OUTPUT);
+    pinMode(D6, OUTPUT);
+    pinMode(D7, OUTPUT);
+    pinMode(D8, OUTPUT);
 }
 
 void loop(){

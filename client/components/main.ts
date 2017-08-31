199cb798f583d25b3b26d532a876286a0c25964f
Rafal Pysz <kuchasz1@o2.pl>
2017-08-31 20:28:05 +0200

    unused component removal;

--- client/components/main.ts
+++ client/components/main.ts
@@ -1,12 +1,8 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { NetworksConfigComponent } from "./networks-config";
 
 @Component({
-    template: ` <div><h3>Main component here!!</h3><router-link to="/config">Show config</router-link></div>`,
-    components: {
-        'network-config-component': NetworksConfigComponent
-    }
+    template: ` <div><h3>Main component here!!</h3><router-link to="/config">Show config</router-link></div>`
 })
 export class MainComponent extends Vue{
  

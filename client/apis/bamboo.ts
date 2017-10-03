859b11d0dc392f17acf484b4724073a5a0b4f5c6
Rafau <kuchasz1@o2.pl>
2017-10-03 23:49:45 +0200

    bammbo configurations extended

--- client/apis/bamboo.ts
+++ client/apis/bamboo.ts
@@ -1,8 +1,23 @@
 const fakeBambooProjectConfig = {
-    url: 'http://www.onet.pl',
-    login: 'admin',
-    password: 'admin'
+    url: 'bamboo.sample.net',
+    login: 'john.doe',
+    password: '******',
+    connected: true,
+    project: '',
+    plan: ''
 };
+
+const projects = ['Aspen', 'Medivio', 'Silvermedic', 'WCA', 'KGHM'];
+const plans = ['Innergy-dev', 'Innergy-test', 'Innergy-beta'];
+
+export interface BambooConfig{
+    url: string;
+    login: string;
+    password: string;
+    connected: boolean;
+    project: string;
+    plan: string;
+}
 
 export const connect = (bambooProjectUrl: string, login: string, password: string) => new Promise<void>((res, rej)=>{
     setTimeout(() => {
@@ -10,5 +25,37 @@ export const connect = (bambooProjectUrl: string, login: string, password: strin
         if (fakeBambooProjectConfig.login !== login) rej();
         if (fakeBambooProjectConfig.password !== password) rej();
         res();
+    }, 250);
+});
+
+export const selectProject = (project: string) => new Promise<void>(res => {
+    setTimeout(() => {
+        fakeBambooProjectConfig.project = project;
+        if (project===project) res();
+    }, 250);
+});
+
+export const selectPlan = (plan: string) => new Promise<void>(res => {
+    setTimeout(()=>{
+        fakeBambooProjectConfig.plan = plan;
+        if (plan === plan) res();
+    }, 250);
+});
+
+export const getBambooConfig = () => new Promise<BambooConfig>((res)=>{
+    setTimeout(()=>{
+        res(fakeBambooProjectConfig);
+    }, 250);
+});
+
+export const getBambooProjects = () => new Promise<string[]>((res) => {
+    setTimeout(()=>{
+        res(projects)
+    }, 250);
+});
+
+export const getBambooPlans = (project: string) => new Promise<string[]>((res)=>{
+    setTimeout(()=>{
+        res(plans.filter(()=>project===project));
     }, 250);
 });
--- client/components/bamboo-config.tsx
+++ client/components/bamboo-config.tsx
@@ -1,5 +1,6 @@
 import React from 'preact';
-import { NetworkConfig, getNetworkConfig } from '../apis/networks';
+import { NetworkConfig, getNetworkConfig, NetworkConnectionStatus } from '../apis/networks';
+import { BambooConfig, getBambooConfig, getBambooProjects, getBambooPlans, selectProject, selectPlan } from '../apis/bamboo';
 
 interface Props{
 
@@ -7,16 +8,57 @@ interface Props{
 
 interface State{
     networkConfig: NetworkConfig;
+    bambooConfig: BambooConfig;
+    projects: string[];
+    plans: string[];
 }
 
 export class BambooConfigComponent extends React.Component<Props, State>{
     
+    bambooServerInput: HTMLInputElement;
+    usernameInput: HTMLInputElement;
+    passwordInput: HTMLInputElement;
+
     componentDidMount(){
-        getNetworkConfig().then(networkConfig => this.setState({
-            networkConfig
-        }));
+
+        getNetworkConfig()
+            .then(networkConfig => {
+                this.setState({networkConfig});
+                return (networkConfig.status !== NetworkConnectionStatus.Connected) 
+                    ? Promise.reject<undefined>(undefined)
+                    : Promise.resolve();
+        }).then(this._getBambooConfig.bind(this));
+
+    }
+
+    _getBambooConfig(){
+        getBambooConfig()
+            .then(bambooConfig => {
+                this.setState({bambooConfig});
+                this.bambooServerInput.value = bambooConfig.url;
+                this.usernameInput.value = bambooConfig.login;
+                this.passwordInput.value = bambooConfig.password;
+
+                (bambooConfig.connected) &&
+                    getBambooProjects().then(projects => {
+                        this.setState({projects})
+                    });
+                (bambooConfig.connected && bambooConfig.project) &&
+                    getBambooPlans(bambooConfig.project).then(plans=>{
+                        this.setState({plans});
+                    });
+            });
     }
     
+    onSelectProject(event: any){
+        selectProject(event.target.value);
+        this._getBambooConfig();
+    }
+
+    onSelectPlan(event: any){
+        selectPlan(event.target.value);
+    }
+
     render(){
         return (
             <div class="bamboo-config-component">
@@ -29,19 +71,30 @@ export class BambooConfigComponent extends React.Component<Props, State>{
                 
                 <div class="input-group">
                     <label>Bamboo server url</label>
-                    <input></input>
+                    <input ref={(el: HTMLInputElement) => this.bambooServerInput = el}></input>
                 </div>
                 <div class="input-group">
                     <label>Username</label>
-                    <input></input>
+                    <input ref={(el: HTMLInputElement) => this.usernameInput = el}></input>
                 </div>
                 <div class="input-group">
                     <label>Password</label>
-                    <input></input>
+                    <input ref={(el: HTMLInputElement) => this.passwordInput = el}></input>
                 </div>
                 <div>
                     <button>Login</button>
                 </div>
+                {this.state.bambooConfig ? <div>
+                    {this.state.projects ? <select onChange={this.onSelectProject.bind(this)} placeholder="">
+                        <option value="">Select project</option>
+                        {this.state.projects.map(p => <option value={p} selected={p===this.state.bambooConfig.project}>{p}</option>)}
+                        </select> : null}
+                    {this.state.plans && this.state.bambooConfig.project ? <select onChange={this.onSelectPlan.bind(this)} placeholder="">
+                        <option value="">Select plan</option>
+                        {this.state.plans.map(p => <option value={p} selected={p===this.state.bambooConfig.plan}>{p}</option>)}
+                        </select> : null}
+                    </div> : null}
+                    
             </div>);
     }
 }
